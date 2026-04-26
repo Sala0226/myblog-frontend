fdc8c2fae8a81b828d89f974a9143d4ac677bdf1
Sala1225 <salalearn12@gmail.com>
2026-04-26 20:10:43 +0000

    Fix: Utilisation des variables d'environnement pour l'API

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api/auth' });
+// const API = axios.create({ baseURL: 'http://localhost:5000/api/auth' });
+// const API = axios.create({ baseURL: 'https://myblog-api-oq9i.onrender.com/api' });
+const API = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api' });
 
 export const login = (data) => API.post('/login', data);
 export const register = (data) => API.post('/register', data);
